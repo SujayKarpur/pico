b8156f9387038005a2e7d7460d33bde4d4485b94
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2026-03-12 06:43:30 +0000

    Implement insert(), erase(), and contains()

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-#include <skippy.h>
+#include "skippy.h"
+
+
+// v vague roadmap
+// single-threaded, locked, and lock-free skip lists //wait free?
+// simple memtable 
+// 3 bench suites -- mixed, read-heavy, and write-heavy 
+// maybe map/umap too idk 
+
+//usage could be like
+    // ./pico 
+    // ./pico -bench 
--- skippy.cpp
+++ skippy.cpp
@@ -1,16 +1,106 @@
-#include <iostream> 
 #include <vector>
 #include <list> 
+#include <limits> 
+#include <random> 
+
+const int INF = std::numeric_limits<int>::max(); 
+const int NUM_LEVELS = 64;
+const int MAX_LEVEL = NUM_LEVELS - 1; 
+
+
+//black box function that returns true 50% of the time, false 50% of the time 
+bool coin_toss(void)
+{
+    static std::random_device rd; 
+    static std::mt19937 gen(rd()); 
+    static std::bernoulli_distribution dist(0.5);
+    
+    return dist(gen); 
+}
+
+
+struct node 
+{
+    int key; 
+    int height; 
+    node *forward[NUM_LEVELS]; 
+
+    node(int v, int h=0): key(v)
+    {
+        height = h; 
+        for (int i=0; i<NUM_LEVELS; i++)
+            forward[i] = nullptr; 
+    }
+};
+
 
 //simple, single-threaded skip list implementing an ordered set interface 
-class SkipList
+class skiplist
 {
     //what do i need 
     //vector of linkedlists ish 
+    private: 
+        node *head = new node(-INF, MAX_LEVEL);
 
-    bool contains(int key); 
-    void insert(int key); 
-    void erase(int key); 
+    public: 
+
+    void insert(int key)
+    {
+        if (contains(key))
+            return ; 
+
+        int height = 0; 
+        while (height<MAX_LEVEL && coin_toss())
+            height++; 
+        
+        node *fresh = new node(key, height); 
+        node *current = head; 
+        for (int level=height; level >= 0; level--) 
+        {
+            while (current->forward[level] != nullptr && key > (current->forward[level])->key)
+                current = current->forward[level]; 
+            
+            node *temp = current->forward[level]; 
+            current->forward[level] = fresh; 
+            fresh->forward[level] = temp; 
+        }
+    }
+
+    bool contains(int key)
+    {
+        node *current = head; 
+        for (int level=MAX_LEVEL; level >= 0; level--)
+        {
+            while (current->forward[level] != nullptr && (current->forward[level])->key)
+                current = current->forward[level]; 
+
+            if (current->forward[level] != nullptr && (current->forward[level])-> key == key)
+                return true; 
+        }
+    }
+
+
+    void erase(int key)
+    {
+        if (!contains(key))
+            return ; 
+
+        node *current = head; 
+        for (int level=MAX_LEVEL; level >= 0; level--)
+        {
+            while (current->forward[level] != nullptr && (current->forward[level])->key)
+                current = current->forward[level]; 
+
+            if (current->forward[level] != nullptr && (current->forward[level])-> key == key)
+            {
+                node *temp = current->forward[level]; 
+                current->forward[level] = temp->forward[level]; 
+
+                if (level == 0)
+                    delete temp; 
+            }
+        }
+    }
 
 }; 
 
